2821fa68ffdd4647281ceaff4ad0db43fd9b5b1b
Kemal Gencay <kemalgencay@gmail.com>
2026-02-28 12:35:53 +0300

    Corrected import

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import "$std/dotenv/load.ts";
 
-import { start } from "fresh";
+import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
 
