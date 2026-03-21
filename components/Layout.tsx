4a26354de1fcc35d85a5caec84332ad2500ce446
kemalgencay <kemalgencay@gmail.com>
2026-03-21 10:08:40 +0300

    update to Fresh 2

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,9 @@ export default function layouts({ children }: any) {
       <nav class="tw w-full px-4 py-3 bg-blue-800 text-white mb-10">
         <a class="tw mx-4" href="/">Ana sayfa</a>
         <a class="tw mx-4" href="/hakkimda">Hakkımda</a>
-        <a class="tw mx-4" href="https://blog-88.kemalgencay.deno.net/">Yazılar</a>
+        <a class="tw mx-4" href="https://blog-88.kemalgencay.deno.net/">
+          Yazılar
+        </a>
         <a class="tw mx-4" href="/sozler">Sözler</a>
       </nav>
       {children}
